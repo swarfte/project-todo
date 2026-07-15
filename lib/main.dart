dfc7428de302a7b073c65b0d45f0fe31e025b0be
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2026-07-15 20:22:03 +0800

    init project

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Project Todo',
+      theme: ThemeData(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Project Todo'),
+        backgroundColor: Colors.blue[600],
+        // set text color to white
+        foregroundColor: Colors.white,
+      ),
+      body: Center(child: Column()),
+    );
+  }
+}
